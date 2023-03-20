4d4330d4a0c1e30741918b4c5adac036d9711f6a
rikumiura <rikumiura0420@gmail.com>
2023-03-20 12:24:27 +0900

    チュートリアル1章終了

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -1,34 +1,42 @@
-// Define a tuple struct
-#[derive(Debug)]
-struct KeyPress(String, char);
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
 
-// Define a classic struct
-#[derive(Debug)]
-struct MouseClick { x: i64, y: i64 }
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    // todo!("Fix enum definition so code compiles");
+    Manual,
+    SemiAuto,
+    Automatic
+}
 
-// Define the WebEvent enum variants to use the data from the structs
-// and a boolean type for the page Load variant
-#[derive(Debug)]
-enum WebEvent { WELoad(bool), WEClick(MouseClick), WEKeys(KeyPress) }
+// Build a "Car" by using values from the input arguments
+// - Color of car (String)
+// - Transmission type (enum value)
+// - Convertible (boolean, true if car is a convertible)
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+
+    // Use the values of the input arguments
+    // All new cars always have zero mileage
+    // let car: Car = todo!("Create an instance of a `Car` struct");
+    let car = Car{color:color, transmission:transmission, convertible:convertible, mileage:0};
+    return car;
+}
 
 fn main() {
-    // Instantiate a MouseClick struct and bind the coordinate values
-    let click = MouseClick { x: 100, y: 250 };
-    println!("Mouse click location: {}, {}", click.x, click.y);
-        
-    // Instantiate a KeyPress tuple and bind the key values
-    let keys = KeyPress(String::from("Ctrl+"), 'N');
-    println!("\nKeys pressed: {}{}", keys.0, keys.1);
-        
-    // Instantiate WebEvent enum variants
-    // Set the boolean page Load value to true
-    let we_load = WebEvent::WELoad(true);
-    // Set the WEClick variant to use the data in the click struct
-    let we_click = WebEvent::WEClick(click);
-    // Set the WEKeys variant to use the data in the keys tuple
-    let we_key = WebEvent::WEKeys(keys);
-        
-    // Print the values in the WebEvent enum variants
-    // Use the {:#?} syntax to display the enum structure and data in a readable form
-    println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key);
+    // We have orders for three new cars!
+    // We'll declare a mutable car variable and reuse it for all the cars
+    let mut car = car_factory(String::from("Red".to_string()), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver".to_string()), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow".to_string()), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);    
 }
