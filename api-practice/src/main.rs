4770f0f55704d242e3b53ecd8d049fbbc3f8511c
rikumiura <rikumiura0420@gmail.com>
2023-03-27 03:07:10 +0900

    envファイルを使用してAPIを叩く

--- api-practice/src/main.rs
+++ api-practice/src/main.rs
@@ -1,66 +1,20 @@
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let res = reqwest::get("https://www.google.co.jp/").await?.text().await?;
-//     println!("{:?}", res);
-//     Ok(())
-// }
+use std::env;
+use std::process;
 
-use anyhow;
-use oauth2::{
-    AuthorizationCode,
-    AuthUrl,
-    ClientId,
-    ClientSecret,
-    CsrfToken,
-    PkceCodeChallenge,
-    RedirectUrl,
-    Scope,
-    TokenResponse,
-    TokenUrl
-};
-use oauth2::basic::BasicClient;
-use oauth2::reqwest::http_client;
-use url::Url;
+fn main() -> std::io::Result<()> {
+    // let env_variables = read_file("src/.env")?;
+    // assert_eq!(&env_variables["KEY"], "YOUR_CLIENT_ID");
 
-// Create an OAuth2 client by specifying the client ID, client secret, authorization URL and
-// token URL.
-let client =
-    BasicClient::new(
-        ClientId::new("client_id".to_string()),
-        Some(ClientSecret::new("client_secret".to_string())),
-        AuthUrl::new("http://authorize".to_string())?,
-        Some(TokenUrl::new("http://token".to_string())?)
-    )
-    // Set the URL the user will be redirected to after the authorization process.
-    .set_redirect_uri(RedirectUrl::new("http://redirect".to_string())?);
 
-// Generate a PKCE challenge.
-let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
-
-// Generate the full authorization URL.
-let (auth_url, csrf_token) = client
-    .authorize_url(CsrfToken::new_random)
-    // Set the desired scopes.
-    .add_scope(Scope::new("read".to_string()))
-    .add_scope(Scope::new("write".to_string()))
-    // Set the PKCE code challenge.
-    .set_pkce_challenge(pkce_challenge)
-    .url();
-
-// This is the URL you should redirect the user to, in order to trigger the authorization
-// process.
-println!("Browse to: {}", auth_url);
-
-// Once the user has been redirected to the redirect URL, you'll have access to the
-// authorization code. For security reasons, your code should verify that the `state`
-// parameter returned by the server matches `csrf_state`.
-
-// Now you can trade it for an access token.
-let token_result =
-    client
-        .exchange_code(AuthorizationCode::new("some authorization code".to_string()))
-        // Set the PKCE code verifier.
-        .set_pkce_verifier(pkce_verifier)
-        .request(http_client)?;
-
-// Unwrapping token_result will either produce a Token or a RequestTokenError.
+    let api_key_name = "KEY";
+    let api_key = match env::var(api_key_name){
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}: KEY",err);
+            process::exit(1);
+        },
+    };
+    let api_url = "URL";
+    println!("{}",api_key);
+    Ok(())
+}
