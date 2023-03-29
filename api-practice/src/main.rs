904d747899ff38851c1eac2a50ed989acfcc5310
rikumiura <rikumiura0420@gmail.com>
2023-03-30 01:25:03 +0900

    api keyを記載したenvファイルを読み込めるようになった！

--- api-practice/src/main.rs
+++ api-practice/src/main.rs
@@ -1,20 +1,37 @@
-use std::env;
-use std::process;
+// use std::env;
+// use std::process;
+use dotenv;
+use std::cmp::Ordering;
+
 
 fn main() -> std::io::Result<()> {
     // let env_variables = read_file("src/.env")?;
     // assert_eq!(&env_variables["KEY"], "YOUR_CLIENT_ID");
 
 
-    let api_key_name = "KEY";
-    let api_key = match env::var(api_key_name){
-        Ok(val) => val,
-        Err(err) => {
-            println!("{}: KEY",err);
-            process::exit(1);
-        },
-    };
-    let api_url = "URL";
-    println!("{}",api_key);
+    // let key = "PATH";
+    // match env::var_os(key) {
+    //     Some(paths) => {
+    //         for path in env::split_paths(&paths) {
+    //             println!("'{}'", path.display());
+    //         }
+    //     }
+    //     None => println!("{key} is not defined in the environment.")
+    // }
+
+    // dotenv().ok();
+
+    let mut api_key = "hoge";
+
+    dotenv::from_filename("api.env").ok();
+    let result: Vec<(String, String)> = dotenv::vars().collect();
+    for item in result{
+        let mut comparison = item.0.cmp(&"KEY".to_string());
+        if comparison ==  Ordering::Equal {
+            let mut api_key = item.1.to_string();
+        }
+        println!("{}: {}", item.0, item.1);
+    }
+    println!("key: {}", api_key);
     Ok(())
 }
