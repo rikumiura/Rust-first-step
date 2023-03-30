f703e866cfad8dde78fcfa133809bfebe075c942
rikumiura <rikumiura0420@gmail.com>
2023-03-30 16:50:04 +0900

    envファイルに記載したAPIキーを正しく取得できる様になった．

--- api-practice/src/main.rs
+++ api-practice/src/main.rs
@@ -21,17 +21,26 @@ fn main() -> std::io::Result<()> {
 
     // dotenv().ok();
 
-    let mut api_key = "hoge";
+    // let mut api_key = "hoge".to_string();
+    let mut api_key:String = "hoge".to_string();
+    // println!("{}: {}", api_key, type_of(&api_key));
+
+    fn type_of<T>(_: T) -> String{
+        let a = std::any::type_name::<T>();
+        return a.to_string();
+    }
 
     dotenv::from_filename("api.env").ok();
     let result: Vec<(String, String)> = dotenv::vars().collect();
     for item in result{
-        let mut comparison = item.0.cmp(&"KEY".to_string());
+        let comparison = item.0.cmp(&"KEY".to_string());
         if comparison ==  Ordering::Equal {
-            let mut api_key = item.1.to_string();
+            api_key = item.1.to_string();
         }
-        println!("{}: {}", item.0, item.1);
+        // println!("{}: {}", item.0, type_of(&item.0));
+
     }
+    // println!("key: {}", type_of(&"KEY".to_string()));
     println!("key: {}", api_key);
     Ok(())
 }
